46b1b387e7cd60a32125e639d29f3ffac6494a2a
viniciusv-erpdev <vcvicari@gmail.com>
2026-08-09 12:00:02 -0300

    start refactoring the old system for file uploading

--- analytics/models.py
+++ analytics/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.conf import settings
 
 class AddressCorrection(models.Model):
 
@@ -92,3 +92,33 @@ class CorrectionAudit(models.Model):
 
     def __str__(self):
         return f"Audit {self.logradouro_limpo} @ {self.created_at.isoformat()}"
+
+class Dataset(models.Model):
+    usuario = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="datasets",
+    )
+
+    nome_original = models.CharField(
+        max_length=255
+    )
+
+    arquivo = models.FileField(
+        upload_to="datasets/"
+    )
+
+    quantidade_registros = models.PositiveIntegerField(
+        default=0
+    )
+
+    criado_em = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    atualizado_em = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return self.nome_original
